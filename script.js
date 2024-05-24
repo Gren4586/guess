9ddea83e3de615db4e28856f409bbac179fa6070
Gren4586 <banananut80@gmail.com>
2024-05-24 09:01:00 -0400

    started adding functionality

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-var roundCount = 0;
 var winCount = 0;
+var roundCount = 0;
 
-var roundCounter = document.getElementById('roundCounter');
-var winCounter = document.getElementById('winCounter');
+var roundDisplay = document.getElementById('roundCounter');
+var winDisplay = document.getElementById('winCounter');
+var startButton = document.getElementById('start');
 
-var cards = document.getElementById('cards').children;
+function shuffle() {
+    let cardBox = document.getElementById('cards');
 
-function startNewRound () {
+    /* Make a random card correct */
+    for (const child of cardBox.children) {
+        let rand = Math.floor(Math.random() * 10);
 
+        if (rand === 5) {
+            child.classList.add('correct');
+            break;
+        }
+    }
+
+    /* Add 'incorrect' class to all other cards */
+    for (const kid of cardBox.children) {
+        if (!kid.classList.contains('correct')) {
+            kid.classList.add('incorrect');
+        }
+    }
+
+    /* Hide start button */
+    startButton.style.display('none');
 }
+
+startButton.addEventListener('click', shuffle, false);
