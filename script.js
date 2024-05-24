b509652fe52df4ce3baf6eef347f77093a2dbe34
Gren4586 <banananut80@gmail.com>
2024-05-24 10:54:47 -0400

    continued adding functionality

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ var roundCount = 0;
 var roundDisplay = document.getElementById('roundCounter');
 var winDisplay = document.getElementById('winCounter');
 var startButton = document.getElementById('start');
+var readyMsg = document.getElementById('go');
 
-function shuffle() {
+function shuffle() { /* Function to start new round upon hitting the 'Shuffle Cards' button */
     let cardBox = document.getElementById('cards');
 
     /* Make a random card correct */
@@ -18,15 +19,45 @@ function shuffle() {
         }
     }
 
-    /* Add 'incorrect' class to all other cards */
+    /* Add 'incorrect' class to all other cards, and add 'ready' class */
     for (const kid of cardBox.children) {
+        
+        kid.classList.add('ready');
+
         if (!kid.classList.contains('correct')) {
             kid.classList.add('incorrect');
         }
     }
 
     /* Hide start button */
-    startButton.style.display('none');
+    startButton.classList.add('hidden');
+    /* Increment round counter */
+    roundCount++;
+    roundDisplay.innerHTML = `Rounds played: ${roundCount}`;
+}
+startButton.addEventListener('click', shuffle, false); /* Makes shuffle button do the thing */
+
+
+function pickWrong() {
+    let cardBox = document.getElementById('cards');
+
+    /* Unhide start button */
+    startButton.classList.remove('hidden');
+
+    for (const child of cardBox.children) {
+        if (child.classList.contains('correct')) {
+            child.classList.remove('correct');
+            child.classList.add('correctChosen');
+        } else if (child.classList.contains('incorrect')) {
+            child.classList.remove('incorrect');
+            child.classList.add('incorrectChosen');
+        }
+    }
 }
 
-startButton.addEventListener('click', shuffle, false);
+var wrongChoice = document.getElementsByClassName('incorrect');
+var rightChoice = document.getElementsByClassName('correct');
+
+console.log(wrongChoice);
+
+wrongChoice.addEventListener('click', pickWrong, false);
