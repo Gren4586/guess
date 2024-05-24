7690230180503c80d83acb85c8314d36e76dfe14
Gren4586 <banananut80@gmail.com>
2024-05-24 07:23:19 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var roundCount = 0;
+var winCount = 0;
+
+var roundCounter = document.getElementById('roundCounter');
+var winCounter = document.getElementById('winCounter');
+
+var cards = document.getElementById('cards').children;
+
+function startNewRound () {
+
+}
